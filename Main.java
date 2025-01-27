7d7b644db5f35a3ea321d5afef2f7875b3a931f8
Tring-santhoshkumar <santhoshkumar.a@tringapps.com>
2025-01-27 16:11:15 +0530

    Updated file

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main{
     public static void main(String[] args) {
     System.out.println("Hello World");
-    System.out.println(2*5);
     System.out.println("Hi World");
     for(int i=0;i<5;i++){
       System.out.print(i+"  ");
