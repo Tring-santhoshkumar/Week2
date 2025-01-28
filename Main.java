9375615f23d7f34eb95ef3befc2742a7ed981109
Tring-santhoshkumar <santhoshkumar.a@tringapps.com>
2025-01-28 17:10:43 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-    System.out.println("Hello World");
-    System.out.println("Hi World");
-    for(int i=0;i<5;i++){
-      System.out.print(i+"  ");
-    }
-  }
-}
